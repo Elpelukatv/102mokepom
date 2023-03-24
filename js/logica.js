179321a474d69a735bbe5c3e92c2bf6f1b6648aa
Francis Gamboa <francis_2193@hotmail.com>
2023-03-24 06:37:37 -0500

    if y else id para seleccionar mascotas

--- js/logica.js
+++ js/logica.js
@@ -1,7 +1,19 @@
-function SelecMascotaJugador () {
-    alert('mascota seleccionada')
+function IniciarJuego() {
+    let SelecMascota = document.getElementById('BotonSelec')
+SelecMascota.addEventListener('click', SelecMascotaJugador)
+}
+function SelecMascotaJugador (){
+    if (document.getElementById('RATIHUELLA').checked)
+    alert('ratiguella seleccionado')
+    else if (document.getElementById('HIPODOGE').checked)
+    alert('hipodoge seleccionado')
+    else if (document.getElementById('CAPIPEPO').checked)
+    alert('capipepo seleccionado')
+    else alert ('selecciona una mascota')
 }
 
-let SelecMascota = document.getElementById('BotonSelec')
-SelecMascota.addEventListener('click', SelecMascotaJugador)
+
+
+
+window.addEventListener('load',IniciarJuego)
 
