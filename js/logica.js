75184667e045e58cb9ce30b3fb2cc849db707004
Francis Gamboa <francis_2193@hotmail.com>
2023-03-31 07:47:06 -0500

    finalizacion de las clases de html y javascript falta solucion de ocultar secciones

--- js/logica.js
+++ js/logica.js
@@ -1,11 +1,22 @@
 let AtackJugador 
 let AtackEnemigo
+let Vida_Jugador =3
+let Vida_Enemigo =3
+
+    // SecSelcMAS       1ER seccion del HTML //
+    // SecSelcATK       2DA seccion del HTML //
+    // SecSelcSMS       3RA seccion del HTML //
+    // SecSelcReinicio  4TA seccion del HTML //
 
     // AF funcion ataque fuego //
     // AA  funcion ataque agua //
     // AT  funcion ataque tierra //
 
 function IniciarJuego() {
+
+    let SecSelcATK = document.getElementById ('SelecAtack')
+    SecSelcATK.style.display ='none'
+
     let SelecMascota = document.getElementById('BotonSelec')
     SelecMascota.addEventListener('click', SelecMascotaJugador)
 
@@ -15,11 +26,17 @@ function IniciarJuego() {
     BTAgua.addEventListener('click', AA)
     let BTTierra = document.getElementById('BTTierra')
     BTTierra.addEventListener('click', AT)
-    SelecMascotaJugador ()
-}
 
-            
+    let reinicio = document.getElementById('again')
+    reinicio.addEventListener('click',reiniciar)
+
+    SelecMascotaJugador ()
+}           
 function SelecMascotaJugador (){
+
+    let SecSelcATK = document.getElementById ('SelecAtack')
+    SecSelcATK.style.display = 'block'
+
     let inputRATIHUELLA = document.getElementById('RATIHUELLA')
     let inputHIPODOGE = document.getElementById('HIPODOGE')
     let inputCAPIPEPO = document.getElementById('CAPIPEPO')
@@ -36,7 +53,6 @@ function SelecMascotaJugador (){
     } 
     SelecMascotaEnemigo ()
 }        
-
 function SelecMascotaEnemigo () {
     let AtackRandom = aleatorio(1,3)
     let spannMascotaEnemigo = document.getElementById('MascotaEnemigo')
@@ -48,8 +64,7 @@ function SelecMascotaEnemigo () {
     } else if (AtackRandom == 3) {
         spannMascotaEnemigo.innerHTML = 'CAPIPEPO'
     }    
-}
-            
+}           
 function AF (){
     AtackJugador = 'FUEGO' 
     AtackEnemigoRandom()
@@ -62,7 +77,6 @@ function AT (){
     AtackJugador = 'TIERRA'
     AtackEnemigoRandom()    
 }
-
 function AtackEnemigoRandom () {
     let AtackRandom2 = aleatorio (1,3)
                     
@@ -78,18 +92,40 @@ function AtackEnemigoRandom () {
 }
 
 function combate (){
+    let SpanVidasJugador = document.getElementById('VidaJugador')
+    let SpanVidasEnemigo = document.getElementById('VidaEnemigo')
+
     if (AtackEnemigo == AtackJugador) {
         createsms("empate")
     } else if (AtackJugador == 'FUEGO' && AtackEnemigo == 'TIERRA') {
         createsms("GANASTE")
+        Vida_Enemigo --
+       SpanVidasEnemigo.innerHTML = Vida_Enemigo
     } else if (AtackJugador == 'AGUA' && AtackEnemigo == 'FUEGO') {
         createsms("GANASTE")
+        Vida_Enemigo --
+       SpanVidasEnemigo.innerHTML = Vida_Enemigo
     } else if (AtackJugador == 'TIERRA' && AtackEnemigo == 'AGUA') {
         createsms("GANASTE")
+        Vida_Enemigo --
+       SpanVidasEnemigo.innerHTML = Vida_Enemigo
     } else {
        createsms("perdiste")
+       Vida_Jugador --
+       SpanVidasJugador.innerHTML = Vida_Jugador
     }
+    revisarvidas()
+
 } 
+
+function revisarvidas(){
+    if (Vida_Jugador == 0){
+        smsEnd ("  ❌❌LO SIENTO PERDISTE❌❌  ")
+    } else if (Vida_Enemigo == 0){
+        smsEnd (" ✅✅  FELICIDADES GANASTE ✅✅   ")
+    }
+    reinicio()
+}
 
 function createsms(resultado){
     let getsms = document.getElementById ('sms') 
@@ -98,6 +134,26 @@ function createsms(resultado){
                         
     getsms.appendChild(parrafo)
 
+}
+
+function smsEnd(resultadoFinal){
+    let getsms = document.getElementById ('sms') 
+    let parrafo = document.createElement('p') 
+    parrafo.innerHTML = resultadoFinal
+                        
+    getsms.appendChild(parrafo)
+
+    let BTFuego = document.getElementById('BTFuego')
+    BTFuego.disabled = true
+    let BTAgua = document.getElementById('BTAgua')
+    BTAgua.disabled = true
+    let BTTierra = document.getElementById('BTTierra')
+    BTTierra.disabled = true
+
+}
+
+function reiniciar(){
+    reinicio = location.reload ()
 }
 
 function aleatorio(min, max) {
