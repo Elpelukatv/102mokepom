18b94cfe6076ffb1309cb4a89a60d44052a52386
Francis Gamboa <francis_2193@hotmail.com>
2023-04-06 06:42:24 -0500

    centrada la segunda pantalla botones de ataque

--- js/logica.js
+++ js/logica.js
@@ -19,6 +19,10 @@ function IniciarJuego() {
     let windows3 = document.getElementById('Jugarotravez')
     windows3.style.display = 'none'
 
+    let windows4 = document.getElementById('vidas')
+    windows4.style.display = 'none'
+
+
     let SelecMascota = document.getElementById('BotonSelec')
     SelecMascota.addEventListener('click', SelecMascotaJugador)
 
@@ -40,6 +44,9 @@ function SelecMascotaJugador (){
     
     let windows2 = document.getElementById('SelecAtack')
     windows2.style.display = 'flex'
+
+    let windows4 = document.getElementById('vidas')
+    windows4.style.display = 'flex'
     
     let inputRATIHUELLA = document.getElementById('RATIHUELLA')
     let inputHIPODOGE = document.getElementById('HIPODOGE')
